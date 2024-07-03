8f0086510b060ae65dbace5240a79c638c101a17
grobin05 <77857662+grobin05@users.noreply.github.com>
2024-07-03 16:26:41 -0700

    Update ControlPanel.java

--- ControlPanel.java
+++ ControlPanel.java
@@ -1,2 +1,16 @@
-public class ControlPanel {
+import javax.swing.*;
+import java.awt.*;
+
+
+public class ControlPanel extends JPanel {
+    public ControlPanel(GridPanel grid) {
+        setBackground(Color.BLUE);
+        JButton clear = new JButton("Clear");
+        JButton next = new JButton("Next");
+        add(clear);
+        add(next);
+        Nanny n1 = new Nanny(grid);
+        clear.addActionListener(n1);
+        next.addActionListener(n1);
+    }
 }
